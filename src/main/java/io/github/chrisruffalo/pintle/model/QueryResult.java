d31a69a14ff3b1cc97ce5cfc539d304bf0ed1a4f
Chris Ruffalo <chris.ruffalo@gmail.com>
2023-10-27 21:26:06 -0400

    [documentation] - added comments to QueryResult enumeration - added comments to ServiceType enumeration

--- src/main/java/io/github/chrisruffalo/pintle/model/QueryResult.java
+++ src/main/java/io/github/chrisruffalo/pintle/model/QueryResult.java
@@ -1,17 +1,50 @@
 package io.github.chrisruffalo.pintle.model;
 
+/**
+ * The overall outcome of a query.
+ */
 public enum QueryResult {
 
+    /**
+     * The query was sent to an upstream resolver
+     * source and an answer was returned and sent
+     * to the client.
+     */
     RESOLVED,
 
+    /**
+     * An error was (or errors were) encountered
+     * during the resolution process and a response
+     * may have been sent to the client. (All errors
+     * should end up here regardless of phase. A
+     * response may not be sent.)
+     */
     ERROR,
 
+    /**
+     * The question was received and an acceptable
+     * answer was found in the cache. This answer
+     * was then sent back to the client.
+     */
     CACHED,
 
+    /**
+     * The question was matched against a group that
+     * contained a list that had an entry that caused
+     * the query to be blocked. The answer sent back
+     * to the client would typically be NXDOMAIN.
+     */
     BLOCKED
 
     ;
 
+    /**
+     * A safe way of parsing the result from a string in
+     * cases where a case-insensitive value may be used.
+     *
+     * @param value representation of the result as a string
+     * @return the enumerated value if it exists, otherwise null
+     */
     public static QueryResult fromString(final String value) {
         try {
             return QueryResult.valueOf(value.toUpperCase());
--- src/main/java/io/github/chrisruffalo/pintle/model/ServiceType.java
+++ src/main/java/io/github/chrisruffalo/pintle/model/ServiceType.java
@@ -1,9 +1,19 @@
 package io.github.chrisruffalo.pintle.model;
 
+/**
+ * What type of listener was used
+ * to receive the query
+ */
 public enum ServiceType {
 
+    /**
+     * A listener using UDP was used (datagram socket server)
+     */
     UDP,
 
+    /**
+     * A listener using TCP was used (tcp socket server)
+     */
     TCP
 
     ;
